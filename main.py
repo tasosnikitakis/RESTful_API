1419c84261f80abe069e5007a33f537e8d8df0a2
Tasos <tasosnikic@gmail.com>
2023-07-17 13:59:00 +0300

    First Commit, get requests done

---
+++ main.py
@@ -0,0 +1,72 @@
+from flask import Flask, jsonify, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import random
+
+app = Flask(__name__)
+
+##Connect to Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy()
+db.init_app(app)
+
+
+##Cafe TABLE Configuration
+class Cafe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=True, nullable=False)
+    map_url = db.Column(db.String(500), nullable=False)
+    img_url = db.Column(db.String(500), nullable=False)
+    location = db.Column(db.String(250), nullable=False)
+    seats = db.Column(db.String(250), nullable=False)
+    has_toilet = db.Column(db.Boolean, nullable=False)
+    has_wifi = db.Column(db.Boolean, nullable=False)
+    has_sockets = db.Column(db.Boolean, nullable=False)
+    can_take_calls = db.Column(db.Boolean, nullable=False)
+    coffee_price = db.Column(db.String(250), nullable=True)
+
+    def to_dict(self):
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+    
+
+## HTTP GET - Read Record
+@app.route('/random')
+def randomise():
+    random_cafe = random.choice(db.session.query(Cafe).all())  # Choose random a record object in the database
+    return jsonify(cafe=random_cafe.to_dict())
+
+
+@app.route('/all')
+def all():
+    all_cafes = []
+    for cafe in db.session.query(Cafe).all():
+        all_cafes.append(cafe.to_dict())
+    return jsonify(cafes=all_cafes)
+
+
+@app.route('/search')
+def search():
+    query_location = request.args.get("loc")
+    cafe = db.session.query(Cafe).filter_by(location=query_location).first()
+    if cafe:
+        return jsonify(cafe=cafe.to_dict())
+    else:
+        return jsonify(error={"Not Found": "Sorry, we don't have a cafe at that location."})
+
+
+
+## HTTP POST - Create Record
+
+## HTTP PUT/PATCH - Update Record
+
+## HTTP DELETE - Delete Record
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
